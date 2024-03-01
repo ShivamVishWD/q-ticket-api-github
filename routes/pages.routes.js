9812fbdb4fb035a37a2cd5e061c0b0427283f532
Shivam Vishwakarma <developer2028web@gmail.com>
2024-03-01 14:51:31 +0530

    Base URL changed

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -1,42 +1,48 @@
 const router = require('express').Router();
 
 router.get('/admin/login', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.render('login', {url: baseUrl, usertoken: null, pagename: 'login', pagetitle: 'Login | Admin', loginfor: 'admin', userid: null, profile: null, username: null})
     else
         res.render('dashboard', {url: baseUrl, usertoken: req.session?.userotken, pagename: 'dashboard', pagetitle: 'Q-Ticket | Dashboard', userid: req.session?.userid, profile: req.session?.profile, username: req.session?.username})    
 })
 router.get('/', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.render('login', {url: baseUrl, usertoken: null, pagename: 'login', pagetitle: 'Login | Team', loginfor: 'employee', userid: null, profile: null, username: null})
     else
         res.render('dashboard', {url: baseUrl, usertoken: req.session?.userotken, pagename: 'dashboard', pagetitle: 'Q-Ticket | Dashboard', userid: req.session?.userid, profile: req.session?.profile, username: req.session?.username})    
 })
 router.get('/dashboard', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.redirect('/')
     else
         res.render('dashboard', {url: baseUrl, usertoken: req.session?.userotken, pagename: 'dashboard', pagetitle: 'Q-Ticket | Dashboard', userid: req.session?.userid, profile: req.session?.profile, username: req.session?.username})
 })
 router.get('/projects', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.redirect('/')
     else
         res.render('projects', {url: baseUrl, usertoken: req.session?.userotken, pagename: 'projects', pagetitle: 'Q-Ticket | Projects', userid: req.session?.userid, profile: req.session?.profile, username: req.session?.username})
 })
 router.get('/employees', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.redirect('/')
     else
         res.render('employees', {url: baseUrl, usertoken: req.session?.userotken, pagename: 'employees', pagetitle: 'Q-Ticket | Teams', userid: req.session?.userid, profile: req.session?.profile, username: req.session?.username})
 })
 router.get('/customers', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.redirect('/')
     else
@@ -44,7 +50,8 @@ router.get('/customers', async(req, res) => {
 })
 
 router.get('/tickets', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     if(req.session.userotken == null)
         res.redirect('/')
     else
@@ -52,13 +59,15 @@ router.get('/tickets', async(req, res) => {
 })
 
 router.get('/admin/logout', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     req.session.destroy();
     res.redirect('/')
 })
 
 router.get('/employee/logout', async(req, res) => {
-    const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    // const baseUrl = req.protocol + '://' + req.headers.host + '/';
+    const baseUrl = '/';
     req.session.destroy();
     res.redirect('/')
 })
