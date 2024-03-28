d9c39342c6b7ea65ed23faac3f18b8676d6d99ed
SVish9050 <shivam1@quadrafort.com>
2024-03-28 17:03:05 +0530

    verify password api updated

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,11 +1,13 @@
 const { HandleError } = require("../helper/ErrorHandler");
 const token = require("../middleware/JWT");
 const customerModel = require("../models/customer");
-const projectModel = require('../models/project');
+const projectModel = require("../models/project");
 const bcrypt = require("bcrypt");
-const { customerOnboardMailTemplate } = require("../templates/mail/sendMailTemplates");
+const {
+  customerOnboardMailTemplate,
+} = require("../templates/mail/sendMailTemplates");
 const { sendEmail } = require("../service/Mail");
-require('dotenv/config')
+require("dotenv/config");
 
 const collectionFields = {
   id: "_id",
@@ -18,21 +20,26 @@ const collectionFields = {
   permission: "Permission",
   permissionType: "PermissionType",
   createby: "CreatedBy",
-  updateby: "LastModifiedBy"
+  updateby: "LastModifiedBy",
 };
 
 const customerController = {
   get: async (req, res) => {
     try {
-      let filterObj = {}
-      if(req.query && Object.keys(req.query).length > 0){
-          for(let key in req.query)
-              filterObj[collectionFields[key]]= req.query[key]
+      let filterObj = {};
+      if (req.query && Object.keys(req.query).length > 0) {
+        for (let key in req.query)
+          filterObj[collectionFields[key]] = req.query[key];
       }
 
-      filterObj = { ...filterObj, IsActive: true, IsDeleted: false }
+      filterObj = { ...filterObj, IsActive: true, IsDeleted: false };
 
-      const result = await customerModel.find(filterObj).populate('Project').populate('CreatedBy').populate('LastModifiedBy').select('-Password');
+      const result = await customerModel
+        .find(filterObj)
+        .populate("Project")
+        .populate("CreatedBy")
+        .populate("LastModifiedBy")
+        .select("-Password");
       return res
         .status(200)
         .json({ status: 200, message: "Records Fetched", data: result });
@@ -43,13 +50,13 @@ const customerController = {
 
   count: async (req, res) => {
     try {
-      let filterObj = {}
-      if(req.query && Object.keys(req.query).length > 0){
-          for(let key in req.query)
-              filterObj[collectionFields[key]]= req.query[key]
+      let filterObj = {};
+      if (req.query && Object.keys(req.query).length > 0) {
+        for (let key in req.query)
+          filterObj[collectionFields[key]] = req.query[key];
       }
 
-      filterObj = { ...filterObj, IsActive: true, IsDeleted: false }
+      filterObj = { ...filterObj, IsActive: true, IsDeleted: false };
 
       const count = await customerModel.find(filterObj).countDocuments();
       return res
@@ -79,27 +86,35 @@ const customerController = {
           fields: mandatoryFields,
         });
 
-      const result = await customerModel.findOne({ Email: req.body.email }).populate('Project');
+      const result = await customerModel
+        .findOne({ Email: req.body.email })
+        .populate("Project");
       if (result != null) {
         let valid = bcrypt.compareSync(req.body.password, result?.Password);
-        if (valid){
-          const responseObj = {_id : result?._id, name: result?.Name, email: result?.Email, profile: 'customer'};
+        if (valid) {
+          const responseObj = {
+            _id: result?._id,
+            name: result?.Name,
+            email: result?.Email,
+            profile: "customer",
+          };
           let jwtToken = await token.generateToken(responseObj);
           req.session.userotken = jwtToken?.token;
           req.session.userid = result?._id;
-          req.session.profile = 'customer';
+          req.session.profile = "customer";
           req.session.username = result?.Name;
-          return res
-            .status(200)
-            .json({
-              status: 200,
-              message: "Login Successful",
-              user: { ...responseObj, project: {
+          return res.status(200).json({
+            status: 200,
+            message: "Login Successful",
+            user: {
+              ...responseObj,
+              project: {
                 id: result?.Project?._id,
                 name: result?.Project?.Name,
-              } },
-              token: jwtToken?.token
-            });
+              },
+            },
+            token: jwtToken?.token,
+          });
         }
       }
       return res.status(200).json({
@@ -111,33 +126,96 @@ const customerController = {
     }
   },
 
+  verify: async (req, res) => {
+    try {
+      if (!req.body && Object.keys(req.body).length == 0)
+        return res
+          .status(200)
+          .json({ status: 400, message: "Request Body should not be empty" });
+
+      let mandatoryFields = [];
+
+      if (!req.body.oldPassword) mandatoryFields.push("oldPassword");
+      if (!req.body.newPassword) mandatoryFields.push("newPassword");
+
+      if (mandatoryFields.length > 0)
+        return res.status(200).json({
+          status: 400,
+          message: "Mandatory Fields Error",
+          fields: mandatoryFields,
+        });
+
+      const getCustomer = await customerModel.findOne({
+        _id: req?.authData?._id,
+      });
+
+      if (
+        getCustomer &&
+        bcrypt.compareSync(req.body.oldPassword, getCustomer?.Password)
+      ) {
+        const result = await customerModel.findOneAndUpdate(
+          { _id: getCustomer?._id },
+          { Password: bcrypt.hashSync(req.body.newPassword, 10) }
+        );
+        if (result)
+          return res
+            .status(200)
+            .json({ status: 200, message: "Password updated" });
+        else
+          return res
+            .status(200)
+            .json({ status: 400, message: "Password not updated" });
+      }
+      else{
+        return res
+            .status(200)
+            .json({ status: 401, message: "Password do not match" });
+      }
+    } catch (error) {
+      return HandleError(error);
+    }
+  },
+
   update: async (req, res) => {
     try {
-      if(req?.authData && req?.authData?.profile != 'admin')
-        return res.status(200).json({status: 400, message: 'Permission Denied'});
-      
+      if (
+        req?.authData &&
+        (req?.authData?.profile != "admin" ||
+          req?.authData?.profile != "customer")
+      )
+        return res
+          .status(200)
+          .json({ status: 400, message: "Permission Denied" });
+
       let filterObj = {};
       let updateObj = {};
 
-      if(!req.query || Object.keys(req.query).length == 0)
-          return res.status(200).json({status: 400, message: 'Filter crieteria missing'});
+      if (!req.query || Object.keys(req.query).length == 0)
+        return res
+          .status(200)
+          .json({ status: 400, message: "Filter crieteria missing" });
 
-      if(!req.body || Object.keys(req.body).length == 0)
-          return res.status(200).json({status: 400, message: 'Update field is missing'});
+      if (!req.body || Object.keys(req.body).length == 0)
+        return res
+          .status(200)
+          .json({ status: 400, message: "Update field is missing" });
 
-      for(let key in req.query)
-          filterObj[collectionFields[key]] = req.query[key]
-      
-      for(let key in req.body)
-          updateObj[collectionFields[key]] = req.body[key]
+      for (let key in req.query)
+        filterObj[collectionFields[key]] = req.query[key];
 
-      updateObj[collectionFields['updateby']] = req?.authData?._id;
-      const result = await customerModel.findOneAndUpdate(filterObj,updateObj,{new: true}).exec();
-      if(result?._id)
-          return res.status(200).json({status: 200, message: 'Record updated'});
+      for (let key in req.body)
+        updateObj[collectionFields[key]] = req.body[key];
+
+      updateObj[collectionFields["updateby"]] = req?.authData?._id;
+      const result = await customerModel
+        .findOneAndUpdate(filterObj, updateObj, { new: true })
+        .exec();
+      if (result?._id)
+        return res.status(200).json({ status: 200, message: "Record updated" });
       else
-          return res.status(200).json({status: 400, message: 'Record updation failed'});
-      
+        return res
+          .status(200)
+          .json({ status: 400, message: "Record updation failed" });
     } catch (error) {
       return HandleError(error);
     }
@@ -145,22 +223,38 @@ const customerController = {
 
   delete: async (req, res) => {
     try {
-      if(req?.authData && req?.authData?.profile != 'admin')
-        return res.status(200).json({status: 400, message: 'Permission Denied'});
-      
+      if (req?.authData && req?.authData?.profile != "admin")
+        return res
+          .status(200)
+          .json({ status: 400, message: "Permission Denied" });
+
       let filterObj = {};
 
-      if(!req.params || Object.keys(req.params).length == 0)
-          return res.status(200).json({status: 400, message: 'Filter crieteria missing'});
+      if (!req.params || Object.keys(req.params).length == 0)
+        return res
+          .status(200)
+          .json({ status: 400, message: "Filter crieteria missing" });
 
-      for(let key in req.query)
-          filterObj[collectionFields[key]] = req.query[key]
+      for (let key in req.query)
+        filterObj[collectionFields[key]] = req.query[key];
 
-      const result = await customerModel.findOneAndUpdate(filterObj, {IsDeleted: true, IsActive: false, LastModifiedBy: req?.authData?._id}, {new: true}).exec();
-      if(result?._id)
-          return res.status(200).json({status: 200, message: 'Record deleted'});
+      const result = await customerModel
+        .findOneAndUpdate(
+          filterObj,
+          {
+            IsDeleted: true,
+            IsActive: false,
+            LastModifiedBy: req?.authData?._id,
+          },
+          { new: true }
+        )
+        .exec();
+      if (result?._id)
+        return res.status(200).json({ status: 200, message: "Record deleted" });
       else
-          return res.status(200).json({status: 400, message: 'Record deletion failed'});
+        return res
+          .status(200)
+          .json({ status: 400, message: "Record deletion failed" });
     } catch (error) {
       return HandleError(error);
     }
@@ -168,9 +262,11 @@ const customerController = {
 
   insert: async (req, res) => {
     try {
-      if(req?.authData && req?.authData?.profile != 'admin')
-        return res.status(200).json({status: 400, message: 'Permission Denied'});
-      
+      if (req?.authData && req?.authData?.profile != "admin")
+        return res
+          .status(200)
+          .json({ status: 400, message: "Permission Denied" });
+
       if (!req.body || Object.keys(req.body).length == 0)
         return res
           .status(200)
@@ -189,8 +285,11 @@ const customerController = {
           fields: mandatoryFields,
         });
 
-      const getData = await customerModel.findOne({Email: req.body.email});
-      if(getData?._id) return res.status(200).json({status: 400, message: 'This Email is already used'});
+      const getData = await customerModel.findOne({ Email: req.body.email });
+      if (getData?._id)
+        return res
+          .status(200)
+          .json({ status: 400, message: "This Email is already used" });
       let decodedPassword = req.body.password;
       req.body.password = bcrypt.hashSync(req.body.password, 10);
       let body = {};
@@ -198,16 +297,37 @@ const customerController = {
         body[collectionFields[key]] = req.body[key];
       }
 
-      body[collectionFields['createby']] = req?.authData?._id;
-      body[collectionFields['updateby']] = req?.authData?._id;
+      body[collectionFields["createby"]] = req?.authData?._id;
+      body[collectionFields["updateby"]] = req?.authData?._id;
       const result = await new customerModel(body).save();
-      const projectUpdate = await projectModel.findOneAndUpdate({_id: req.body.project}, {Customer: result?._id}, {new: true}).exec();
-      if(result?._id){
-        if(process.env.ENVIRONMENT != 'DEV')
-          sendEmail(result?.Email, 'Welcome to Q-Ticket', '', customerOnboardMailTemplate(result?.Name, result?.Email, result?.Email, decodedPassword, projectUpdate?.Name))
-        return res.status(200).json({ status: 200, message: "Customer Inserted", data: result });
-      }else
-        return res.status(200).json({staus: 400, message: 'Customer Insertion failed'});
+      const projectUpdate = await projectModel
+        .findOneAndUpdate(
+          { _id: req.body.project },
+          { Customer: result?._id },
+          { new: true }
+        )
+        .exec();
+      if (result?._id) {
+        if (process.env.ENVIRONMENT != "DEV")
+          sendEmail(
+            result?.Email,
+            "Welcome to Q-Ticket",
+            "",
+            customerOnboardMailTemplate(
+              result?.Name,
+              result?.Email,
+              result?.Email,
+              decodedPassword,
+              projectUpdate?.Name
+            )
+          );
+        return res
+          .status(200)
+          .json({ status: 200, message: "Customer Inserted", data: result });
+      } else
+        return res
+          .status(200)
+          .json({ staus: 400, message: "Customer Insertion failed" });
     } catch (error) {
       return HandleError(error);
     }
--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -7,6 +7,7 @@ router.get('/count', token.verifyToken, customerController.count);
 router.post('/insert', token.verifyToken, customerController.insert);
 router.post('/login', customerController.login);
 router.patch('/update', token.verifyToken, customerController.update);
+router.patch('/verify', token.verifyToken, customerController.verify);
 router.delete('/delete/:id', token.verifyToken, customerController.delete);
 
 module.exports = router;
